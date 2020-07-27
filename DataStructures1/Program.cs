23c64346019daab87264ed8cf957d5ef14ea7aff
WELCOME <WELCOME@DESKTOP-VNDV0B2>
2020-07-27 13:03:08 +0530

    Add project files.

---
+++ DataStructures1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructures1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 23;
+            int b = 112;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+        }
+
+    }
+}
